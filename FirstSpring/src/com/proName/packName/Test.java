299c4f85bf8b641ac498da1be0424b6a88429407
srika <srika@LAPTOP-81P5HPDV>
2020-11-20 12:12:26 +0530

    first Spring

---
+++ FirstSpring/src/com/proName/packName/Test.java
@@ -0,0 +1,19 @@
+package com.proName.packName;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Resource res=new ClassPathResource("applicationContext.xml");
+		BeanFactory factory=new XmlBeanFactory(res);
+		Student std=(Student)factory.getBean("studentbean");
+		
+		std.displayInfo(); //displayInfo() method in Student class
+
+	}
+
+}

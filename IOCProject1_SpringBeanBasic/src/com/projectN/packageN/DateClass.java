334f1fee4b4baffb04075571fb2fc55a67650d9c
srika <srika@LAPTOP-81P5HPDV>
2020-11-24 22:30:17 +0530

    iocpro1

---
+++ IOCProject1_SpringBeanBasic/src/com/projectN/packageN/DateClass.java
@@ -0,0 +1,24 @@
+package com.projectN.packageN;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class DateClass {
+	
+	public static void main(String args[]){
+		/*
+		FileSystemResource res=new FileSystemResource("applicationContext.xml");
+		XmlBeanFactory factory=new XmlBeanFactory(res);*/
+				
+		Resource res=new ClassPathResource("applicationContext.xml");
+		BeanFactory factory=new XmlBeanFactory(res);
+		
+		Date d=(Date)factory.getBean("date");
+		System.out.println("TODAYS DATE ::"+d);
+	}
+	
+}

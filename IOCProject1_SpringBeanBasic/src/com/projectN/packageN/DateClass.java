b41f498afa66cea49b5ffdc69dad4db36900a980
srika <srika@LAPTOP-81P5HPDV>
2020-11-25 21:34:53 +0530

    update

--- IOCProject1_SpringBeanBasic/src/com/projectN/packageN/DateClass.java
+++ IOCProject1_SpringBeanBasic/src/com/projectN/packageN/DateClass.java
@@ -5,14 +5,15 @@ import java.util.Date;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 
 public class DateClass {
 	//IOC -> Inversion of Control
 	
 	public static void main(String args[]){
-		/*
-		FileSystemResource res=new FileSystemResource("applicationContext.xml");
+		
+		/*FileSystemResource res=new FileSystemResource("applicationContext.xml");
 		XmlBeanFactory factory=new XmlBeanFactory(res);*/
 				
 		Resource res=new ClassPathResource("applicationContext.xml");

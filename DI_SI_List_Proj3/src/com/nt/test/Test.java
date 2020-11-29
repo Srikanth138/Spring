0fd6a735ab72308da22ad3e14001f5b2cf660d64
srika <srika@LAPTOP-81P5HPDV>
2020-11-29 14:25:48 +0530

    DI_SI_List

---
+++ DI_SI_List_Proj3/src/com/nt/test/Test.java
@@ -0,0 +1,32 @@
+package com.nt.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.nt.bean.Employee;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext cnt=new ClassPathXmlApplicationContext("applicationContext.xml");
+		Employee emp=(Employee)cnt.getBean("emp");
+		
+		System.out.println(emp); // here toString() method print
+		
+		System.out.println(emp.getClass());
+		System.out.println(emp.getId());
+		System.out.println(emp.getName());
+		System.out.println(emp.getPhno());
+		System.out.println(emp.getSalary().getClass()); //print the class name
+		System.out.println(emp.getSalary());
+		System.out.println(emp.getSalary().get(2)); //print only index value 
+		
+		System.out.println("salary Iterator ::");
+		emp.showList();
+	
+		((AbstractApplicationContext) cnt).close(); //warring are raise because of closing
+		
+	}
+
+}

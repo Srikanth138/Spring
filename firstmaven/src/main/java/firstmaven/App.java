849b6538e5d5820c4238c2708872dfa78f7d99ad
srika <srika@LAPTOP-81P5HPDV>
2020-12-01 09:51:51 +0530

    update

--- firstmaven/src/main/java/firstmaven/App.java
+++ firstmaven/src/main/java/firstmaven/App.java
@@ -1,23 +1,29 @@
 package firstmaven;
 
+
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 
+@SuppressWarnings("deprecation")
 public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Hello First");
-		/*ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");*/
+//		ApplicationContext factory = new ClassPathXmlApplicationContext("config.xml");
 		
-		Resource res=new ClassPathResource("config.xml");
-		BeanFactory factory=new XmlBeanFactory(res);
+		
+		  Resource res=new ClassPathResource("config.xml"); 
+		  BeanFactory factory=new XmlBeanFactory(res);
+		 
 		Test std=(Test)factory.getBean("test1");
-		
 		System.out.println(std.getId());
 		System.out.println(std.getName());
+		
+		
+//		((AbstractApplicationContext) factory).close();
 	}
 
 }

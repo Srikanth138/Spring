aecc21f04a4a344463e526eba22f198b40e89cfd
srika <srika@LAPTOP-81P5HPDV>
2020-12-08 21:15:34 +0530

    ResultSet

---
+++ ResultSetExtractor/src/main/java/com/nt/test/App.java
@@ -0,0 +1,25 @@
+package com.nt.test;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.nt.bean.Employee;
+import com.nt.dao.EmployeeDAO;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	 ApplicationContext ctx=new ClassPathXmlApplicationContext("com/nt/cnfg/applicationContext.xml");  
+    	    EmployeeDAO dao=ctx.getBean("edao",EmployeeDAO.class);  
+    	    List<Employee> list=dao.getAllEmployees();  
+    	          
+    	    for(Employee e:list)  
+    	        System.out.println("Extract THE RESULT..."+e);  
+    	       
+    	    ((AbstractApplicationContext) ctx).close();
+    }  
+}

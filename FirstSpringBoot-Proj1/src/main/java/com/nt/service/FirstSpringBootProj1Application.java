20b654572f1065c277ecba22bc5d9b17e477708a
srika <srika@LAPTOP-81P5HPDV>
2020-12-08 21:44:48 +0530

    FirstSpringBoot_Proj1

---
+++ FirstSpringBoot-Proj1/src/main/java/com/nt/service/FirstSpringBootProj1Application.java
@@ -0,0 +1,16 @@
+
+//Refer -> AopProj16-AspectjAopBoot
+package com.nt.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootProj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootProj1Application.class, args);
+		System.out.println("FirstSpringBootProj1Application.main()");
+	}
+
+}
---
+++ FirstSpringBoot-Proj1/src/test/java/com/nt/service/FirstSpringBootProj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootProj1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

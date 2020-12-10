1fcbef7af09074391117b88d0c6754594c37e125
srika <srika@LAPTOP-81P5HPDV>
2020-12-10 13:03:22 +0530

    update

--- ResultSetExtractor/src/main/java/com/nt/dao/EmployeeDAO.java
+++ ResultSetExtractor/src/main/java/com/nt/dao/EmployeeDAO.java
@@ -13,43 +13,41 @@ import com.nt.bean.Employee;
 
 public class EmployeeDAO {
 	private JdbcTemplate template;
-	
-	
+
 	public void setTemplate(JdbcTemplate template) {
 		this.template = template;
 	}
 
+	public List<Employee> getAllEmployees() {
 
-	public List<Employee> getAllEmployees(){ 
-		
-		//We can easily fetch the records from the database using "query()" method of 
-		//JdbcTemplate class where we need to pass the instance of ResultSetExtractor.
-		
-		 return template.query("select * from emp1",new ResultSetExtractor<List<Employee>>(){
-			 
-		// ResultSetExtractor interface can be used to fetch records from the database. It accepts a ResultSet and returns the list.
-		    
-			 @Override  
-			 //It defines only one method "extractData" that accepts ResultSet instance as a parameter.
-		     public List<Employee> extractData(ResultSet rs) throws SQLException,DataAccessException {  
-		      
-		        List<Employee> list=new ArrayList<Employee>();  
-		        Employee e=new Employee();
-		        
-//		        while(rs!=null){ 
+		// We can easily fetch the records from the database using "query()" method of
+		// JdbcTemplate class where we need to pass the instance of ResultSetExtractor.
+
+		return template.query("select * from emp1", new ResultSetExtractor<List<Employee>>() {
+
+			// ResultSetExtractor interface can be used to fetch records from the database.
+			// It accepts a ResultSet and returns the list.
+
+			@Override
+			// It defines only one method "extractData" that accepts ResultSet instance as a
+			// parameter.
+			public List<Employee> extractData(ResultSet rs) throws SQLException, DataAccessException {
+
+				List<Employee> list = new ArrayList<Employee>();
+				Employee e = new Employee();
+
+//				if(rs != null) {
 		        while(rs.next()){ 
-//		        Employee e=new Employee();
-//		        	rs.next();
-		        e.setId(rs.getInt(1));  
-		        e.setName(rs.getString(2));  
-		        e.setSalary(rs.getInt(3)); 
-		          
-		        //list.add(e);
-		        } 
 		        
-		        list.add(e);
-		        return list;  
-		        }  
-		    });  
-		  }  
+//					if(rs.next())
+					e.setId(rs.getInt(1));
+					e.setName(rs.getString(2));
+					e.setSalary(rs.getInt(3));
+					list.add(e);
+				}
+				return list;
+			}
+		});
+	}
+
 }
--- ResultSetExtractor/src/main/java/com/nt/test/App.java
+++ ResultSetExtractor/src/main/java/com/nt/test/App.java
@@ -9,17 +9,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.nt.bean.Employee;
 import com.nt.dao.EmployeeDAO;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	 ApplicationContext ctx=new ClassPathXmlApplicationContext("com/nt/cnfg/applicationContext.xml");  
-    	    EmployeeDAO dao=ctx.getBean("edao",EmployeeDAO.class);  
-    	    List<Employee> list=dao.getAllEmployees();  
-    	          
-    	    for(Employee e:list)  
-    	        System.out.println("Extract THE RESULT..."+e);  
-    	       
-    	    ((AbstractApplicationContext) ctx).close();
-    }  
+public class App {
+	public static void main(String[] args) {
+		int count = 1;
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("com/nt/cnfg/applicationContext.xml");
+		EmployeeDAO dao = ctx.getBean("edao", EmployeeDAO.class);
+		List<Employee> list = dao.getAllEmployees();
+
+		for (Employee e : list) {
+			System.out.println("Extract THE RESULT From Row's..." + count + " :: " + e);
+			count++;
+		}
+		((AbstractApplicationContext) ctx).close();
+	}
 }

48524228cf3667307955486320e05e6c030761a7
srika <srika@LAPTOP-81P5HPDV>
2020-12-18 21:08:40 +0530

    CyclicDI-SetterInjection-ConstructorInjection

---
+++ IOCProj12-CyclicDI-SetterInjection-ConstructorInjection/src/com/nt/service/A.java
@@ -0,0 +1,20 @@
+package com.nt.service;
+
+public class A {
+	private B b;
+
+	public B getB() {
+		System.out.println("A.getB()");
+		return b;
+	}
+
+	public void setB(B b) {
+		System.out.println("A.setB()");
+		this.b = b;
+	}
+
+	@Override
+	public String toString() {
+		return "A [b=" + b + "]";
+	}
+}

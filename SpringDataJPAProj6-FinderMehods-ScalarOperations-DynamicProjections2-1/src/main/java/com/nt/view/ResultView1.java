ee51b4036da6caa8e7ca8574ad9103f91a2b24fd
srika <srika@LAPTOP-81P5HPDV>
2021-01-05 13:32:24 +0530

    FinderMethod Dynamically

---
+++ SpringDataJPAProj6-FinderMehods-ScalarOperations-DynamicProjections2-1/src/main/java/com/nt/view/ResultView1.java
@@ -0,0 +1,7 @@
+package com.nt.view;
+
+public interface ResultView1 extends View {
+	public   Integer getEno();
+	   public   String  getEadd();
+
+}
---
+++ SpringDataJPAProj6-FinderMehods-ScalarOperations-DynamicProjections2-1/src/main/java/com/nt/view/ResultView3.java
@@ -0,0 +1,9 @@
+package com.nt.view;
+
+public interface ResultView3 extends View {
+	public Integer getEno();
+
+	public String getEname();
+
+	public String getEadd();
+}

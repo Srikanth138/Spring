3135f9de75fdc5c231207c9c8f72d8ee18a788b8
srika <srika@LAPTOP-81P5HPDV>
2021-01-07 11:30:59 +0530

    DataTransfer

---
+++ TxMgmtProj2Anno-LocalTxMgmt-DataSoruceTxMgmr/src/main/java/com/nt/service/IBankMgmtService.java
@@ -0,0 +1,8 @@
+package com.nt.service;
+
+import java.sql.SQLException;
+
+public interface IBankMgmtService {
+	public boolean transferMoney(long srcAcno, long destAcno, double amount) throws SQLException;
+
+}
---
+++ TxMgmtProj2Anno-LocalTxMgmt-DataSoruceTxMgmr/src/main/java/com/nt/test/App.java
@@ -0,0 +1,30 @@
+package com.nt.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.nt.service.IBankMgmtService;
+
+public class App {
+	public static void main(String[] args) {
+		// create IOC container
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("com/nt/cfgs/applicationContext.xml");
+		
+		// get Proxy object
+		IBankMgmtService proxy = ctx.getBean("bankService", IBankMgmtService.class);
+		System.out.println(proxy.getClass());
+		// invoke b.method
+		try {
+			if (proxy.transferMoney(1001, 1002, 2000.0))
+				System.out.println("Money Transfered");
+			else
+				System.out.println("Problem in momeny  transffering");
+		} catch (Exception e) {
+			System.out.println("Money not transffered");
+			e.printStackTrace();
+		}
+		// close container
+		((AbstractApplicationContext) ctx).close();
+	}
+}

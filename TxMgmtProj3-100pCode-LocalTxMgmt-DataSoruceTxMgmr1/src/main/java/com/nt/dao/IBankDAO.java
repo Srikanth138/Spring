d6b00deccbd056e076c1652abdcbcc7a743500e0
srika <srika@LAPTOP-81P5HPDV>
2021-01-07 12:09:39 +0530

    100p Code TransationManagemet

---
+++ TxMgmtProj3-100pCode-LocalTxMgmt-DataSoruceTxMgmr1/src/main/java/com/nt/dao/IBankDAO.java
@@ -0,0 +1,8 @@
+package com.nt.dao;
+
+public interface IBankDAO {
+	public int withdraw(long acno, double amout);
+
+	public int deposite(long acno, double amount);
+
+}
---
+++ TxMgmtProj3-100pCode-LocalTxMgmt-DataSoruceTxMgmr1/src/test/java/cmp/TxMgmtProj3_100pCode_LocalTxMgmt_DataSoruceTxMgmr1/AppTest.java
@@ -0,0 +1,20 @@
+package cmp.TxMgmtProj3_100pCode_LocalTxMgmt_DataSoruceTxMgmr1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}

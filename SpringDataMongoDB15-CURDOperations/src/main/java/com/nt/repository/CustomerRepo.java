c4c99bf6afcebcbe91cdcd0aa340baf6c8fe5d69
srika <srika@LAPTOP-81P5HPDV>
2021-01-24 10:58:44 +0530

    MongoDB

---
+++ SpringDataMongoDB15-CURDOperations/src/main/java/com/nt/repository/CustomerRepo.java
@@ -0,0 +1,13 @@
+package com.nt.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.nt.documnet.Customer;
+
+public interface CustomerRepo extends MongoRepository<Customer, Integer> {
+	public   List<Customer>  findByCadd(String cadd);
+    public  List<Customer>  findByCnoBetween(int start,int end);
+
+}

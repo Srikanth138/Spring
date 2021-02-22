ea62b4aa773b6295e313b4d7507221812fd0283c
srika <srika@LAPTOP-81P5HPDV.mshome.net>
2021-02-22 08:10:41 +0530

    DBFormValidation

---
+++ MVCProjAnno13-CustomerRegistration-FormValidations/src/main/java/com/nt/model/Customer.java
@@ -0,0 +1,13 @@
+package com.nt.model;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+	//private int cno;  //auto generated
+		private String cname;
+		private  String cadd;
+		private  Float billAmt;
+		private  String vflag="no";
+
+}

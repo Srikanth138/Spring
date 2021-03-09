8dc2e30c4fe9a368955e1cda424f5c4e0afee903
srika <srika@LAPTOP-81P5HPDV>
2021-03-09 12:37:07 +0530

    webapp

--- MVCProj26-Boot-FirstWishApp/src/main/java/com/nt/controller/WishMessageController.java
+++ MVCProj26-Boot-FirstWishApp/src/main/java/com/nt/controller/WishMessageController.java
@@ -1,16 +1,16 @@
 package com.nt.controller;
 
+//http://localhost:4040/welcome
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WishMessageController {
 
-	// @RequestMapping("/welcome")
-	@GetMapping("/welcome")
+	 @RequestMapping("/welcome")
+//	@GetMapping("/welcome")
 	public String getValues(Map<String, Object> map) {
 		String msg = "hello... <br> Welcome to first SpringBoot Application";
 		map.put("msg", msg);

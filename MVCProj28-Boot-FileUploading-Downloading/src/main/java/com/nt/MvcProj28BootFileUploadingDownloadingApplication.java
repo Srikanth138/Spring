3a65da681a513f7f589b45c495bb7c357fa6e042
srika <srika@LAPTOP-81P5HPDV>
2021-03-12 21:39:46 +0530

    FileUpload AND download

---
+++ MVCProj28-Boot-FileUploading-Downloading/src/main/java/com/nt/MvcProj28BootFileUploadingDownloadingApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcProj28BootFileUploadingDownloadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcProj28BootFileUploadingDownloadingApplication.class, args);
+	}
+
+}

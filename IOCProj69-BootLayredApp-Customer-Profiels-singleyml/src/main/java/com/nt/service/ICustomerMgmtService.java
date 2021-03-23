0efe3c328b093d954c06632434f2068c8bdd5c69
srika <srika@192.168.0.7>
2021-03-23 23:26:19 +0530

    wrong code

---
+++ IOCProj69-BootLayredApp-Customer-Profiels-singleyml/src/main/java/com/nt/service/ICustomerMgmtService.java
@@ -0,0 +1,9 @@
+package com.nt.service;
+
+import com.nt.dto.CustomerDTO;
+
+public interface ICustomerMgmtService {
+	public String calculateSimpleIntrestAmount(CustomerDTO dto)
+			throws Exception;
+
+}

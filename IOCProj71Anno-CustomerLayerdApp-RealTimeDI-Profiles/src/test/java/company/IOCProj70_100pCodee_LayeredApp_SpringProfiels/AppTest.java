7cae363866993f4b2d54ac7614c26493a9f0efe5
srika <srika@192.168.0.7>
2021-03-24 22:28:11 +0530

    Error Code

---
+++ IOCProj71Anno-CustomerLayerdApp-RealTimeDI-Profiles/src/test/java/company/IOCProj70_100pCodee_LayeredApp_SpringProfiels/AppTest.java
@@ -0,0 +1,20 @@
+package company.IOCProj70_100pCodee_LayeredApp_SpringProfiels;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}

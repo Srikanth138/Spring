8b421168d897b5efe04d9daa19c7ee232a4f3cb2
srika <srika@192.168.0.4>
2021-03-30 00:31:34 +0530

    FirstJDBC

---
+++ SpringJDBC01-JdbcTemplate-CURDOperations-Directmethods/lib/src/main/java/com/nt/test/JdbcTemplateDirectMethodsTest.java
@@ -0,0 +1,19 @@
+package com.nt.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class JdbcTemplateDirectMethodsTest {
+	public static void main(String args[]) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(
+				"com/nt/cnfg/applicationContext.xml");
+		// get Target class object
+		IEmployeeMgmtService service = ctx.getBean("empService",
+				IEmployeeMgmtService.class);
+		// invoke methods
+		System.out.println("EmpsCount::" + service.fetchEmpsCount());
+		// close IOC container
+		((AbstractApplicationContext) ctx).close();
+	}
+
+}

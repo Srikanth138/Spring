ffe9d90aaef869fbe146de53c2220d547f2875a0
srika <srika@192.168.0.4>
2021-03-30 00:51:59 +0530

    Gradle

--- SpringJDBC01-JdbcTemplate-CURDOperations-Directmethods/lib/src/main/java/com/nt/test/JdbcTemplateDirectMethodsTest.java
+++ SpringJDBC01-JdbcTemplate-CURDOperations-Directmethods/lib/src/main/java/com/nt/test/JdbcTemplateDirectMethodsTest.java
@@ -1,7 +1,10 @@
 package com.nt.test;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.nt.service.IEmployeeMgmtService;
 
 public class JdbcTemplateDirectMethodsTest {
 	public static void main(String args[]) {

6c26a38ad5b0616424f948943abe1cf938a11a54
srika <srika@192.168.0.4>
2021-03-29 23:44:37 +0530

    BootGradle

---
+++ MVCProj-OWN-BOOT-Gradle/src/main/java/com/nt/controller/WishApp.java
@@ -0,0 +1,14 @@
+package com.nt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WishApp {
+	@RequestMapping("/wish")
+	public String get() {
+		return "result";
+		
+	}
+
+}
---
+++ MVCProj-OWN-BOOT-Gradle/src/main/webapp/WEB-INF/pages/result.jsp
@@ -0,0 +1,7 @@
+<%@ page language="java" isELIgnored="false" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+ 
+ <h1 style="color: green;text-align: center;">THIS IS RESULT PAGE</h1>
+ 
+ <br></br>
+ <a href="index.html">home</a>

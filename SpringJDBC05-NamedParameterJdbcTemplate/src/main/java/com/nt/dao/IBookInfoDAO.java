1cdab9b4cbc04c05194d014333bcd0846df268be
srika <srika@192.168.0.4>
2021-04-08 22:20:21 +0530

    SpringNamedJDBC

---
+++ SpringJDBC05-NamedParameterJdbcTemplate/src/main/java/com/nt/dao/IBookInfoDAO.java
@@ -0,0 +1,8 @@
+package com.nt.dao;
+
+import com.nt.model.BookInfo;
+
+
+public interface IBookInfoDAO {
+	public BookInfo getBookById(int bookId);
+}
---
+++ SpringJDBC05-NamedParameterJdbcTemplate/src/main/java/com/nt/service/BookStoreMgmtServiceImpl.java
@@ -0,0 +1,22 @@
+package com.nt.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nt.dao.IBookInfoDAO;
+import com.nt.model.BookInfo;
+
+//@Component("bookService")
+@Service("bookService")
+public class BookStoreMgmtServiceImpl implements IBookStoreMgmtService {
+	@Autowired
+	private IBookInfoDAO dao;
+
+	@Override
+	public BookInfo fetchBookById(int bookId) {
+		// use DAO
+		BookInfo info = dao.getBookById(bookId);
+		return info;
+	}
+
+}

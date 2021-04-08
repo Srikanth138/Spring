99c12078f7b2c2fca7ca7313745ace4fb9784cfc
srika <srika@192.168.0.4>
2021-04-08 21:25:06 +0530

    SpringJDBC 100%Code App

---
+++ SpringJDBC03-100pCode-JdbcTemplateCallbackMethods/lib/src/main/java/com/nt/test/CallbackMethodsTest.java
@@ -0,0 +1,45 @@
+package com.nt.test;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.dao.DataAccessException;
+
+import com.nt.cfgs.AppConfig;
+import com.nt.dto.EmployeeDTO;
+import com.nt.service.IEmployeeMgmtService;
+
+public class CallbackMethodsTest {
+
+	public static void main(String[] args) {
+		// create IOC container
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(
+				AppConfig.class);
+		// get Service class obj
+		IEmployeeMgmtService service = ctx.getBean("empService",
+				IEmployeeMgmtService.class);
+		// invoke method
+		try {
+			System.out.println("7499 Emp details are ::"
+					+ service.fetchEmployeeByNo(7499));
+		} catch (DataAccessException dae) {
+			dae.printStackTrace();
+		}
+		System.out.println("=================================");
+		try {
+			System.out.println(
+					"Employees having CLERK,MANAGER,SALESMAN Desgs are ");
+			List<EmployeeDTO> listDTO = service.fetchEmployeesByDesg("CLERK",
+					"MANAGER", "SALESMAN");
+			listDTO.forEach(System.out::println);
+		} catch (DataAccessException dae) {
+			dae.printStackTrace();
+		}
+		// close container
+		((AbstractApplicationContext) ctx).close();
+
+	}
+
+}
